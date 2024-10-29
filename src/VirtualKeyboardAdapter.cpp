363e46514a64e1bb09ae8d8cc57ea5b06ea4e61f
Ching Pei Yang <badnam3o.0@gmail.com>
2024-10-29 12:45:37 +0100

    add debouncer

--- src/VirtualKeyboardAdapter.cpp
+++ src/VirtualKeyboardAdapter.cpp
@@ -1,5 +1,51 @@
 #include "VirtualKeyboardAdapter.hpp"
 
+#include <cstdint>
 #include <fcitx/addoninstance.h>
+#include <fcitx/event.h>
 
 FCITX_ADDON_FACTORY(VirtualKeyboardAdapterFactory);
+
+void VirtualKeyboardAdapter::onActivate(fcitx::Event& event) {
+    switch (this->activeDebounce) {
+    case NONE:
+        this->debouncer->setEnabled(true);
+        this->activeDebounce = ACTIVATE;
+        break;
+    case ACTIVATE:
+        break;
+    case DEACTIVATE:
+        this->debouncer->setEnabled(false);
+        this->activeDebounce = NONE;
+        break;
+    }
+}
+
+void VirtualKeyboardAdapter::onDeactivate(fcitx::Event& event) {
+    switch (this->activeDebounce) {
+    case NONE:
+        this->debouncer->setEnabled(true);
+        this->activeDebounce = DEACTIVATE;
+        break;
+    case ACTIVATE:
+        this->debouncer->setEnabled(false);
+        this->activeDebounce = NONE;
+        break;
+    case DEACTIVATE:
+        break;
+    }
+}
+
+void VirtualKeyboardAdapter::onDebounceComplete(uint64_t) {
+    switch (this->activeDebounce) {
+    case NONE:
+        system("notify-send 'debounce ended with NONE'");
+        break;
+    case ACTIVATE:
+        system("notify-send 'activate'");
+        break;
+    case DEACTIVATE:
+        system("notify-send 'deactivate'");
+        break;
+    }
+}
--- src/VirtualKeyboardAdapter.hpp
+++ src/VirtualKeyboardAdapter.hpp
@@ -1,5 +1,4 @@
 #include <cstdint>
-#include <ctime>
 #include <fcitx-utils/event.h>
 #include <fcitx/addonfactory.h>
 #include <fcitx/addoninstance.h>
@@ -18,31 +17,43 @@ constexpr int MAGIC = 4107;
 class VirtualKeyboardAdapter: public fcitx::AddonInstance {
   public:
     VirtualKeyboardAdapter(fcitx::Instance* instance, fcitx::EventLoop* loop) : instance(instance) {
-        this->timer = std::move(loop->addTimeEvent(
+        this->debouncer = loop->addTimeEvent(
             CLOCK_MONOTONIC,
             VIRTUAL_KEYBOARD_ADAPTER_THROTTLE_USEC,
             1000 /*I have no clue what this does */,
             [this](fcitx::EventSourceTime* _, uint64_t usec) { return false; }
-        ));
+        );
+        this->debouncer->setOneShot();
 
-        this->activateWatcher = this->instance->watchEvent(
+        this->activationWatcher = this->instance->watchEvent(
             fcitx::EventType::InputContextInputMethodActivated,
             fcitx::EventWatcherPhase::PreInputMethod,
-            [this](fcitx::Event& _event) { int _ = system("notify-send activated"); }
+            [this](fcitx::Event& event) { this->onActivate(event); }
         );
 
-        this->deactivateWatcher = this->instance->watchEvent(
+        this->deactivationWatcher = this->instance->watchEvent(
             fcitx::EventType::InputContextInputMethodDeactivated,
             fcitx::EventWatcherPhase::PreInputMethod,
-            [this](fcitx::Event& _event) { int _ = system("notify-send deactivated"); }
+            [this](fcitx::Event& event) { this->onDeactivate(event); }
         );
     }
 
   private:
+    enum DEBOUNCE_TARGET {
+        NONE,
+        ACTIVATE,
+        DEACTIVATE,
+    };
+
     fcitx::Instance* instance;
-    std::unique_ptr<fcitx::EventSourceTime> timer;
-    std::unique_ptr<fcitx::HandlerTableEntry<fcitx::EventHandler>> activateWatcher;
-    std::unique_ptr<fcitx::HandlerTableEntry<fcitx::EventHandler>> deactivateWatcher;
+    DEBOUNCE_TARGET activeDebounce = NONE;
+    std::unique_ptr<fcitx::EventSourceTime> debouncer;
+    std::unique_ptr<fcitx::HandlerTableEntry<fcitx::EventHandler>> activationWatcher;
+    std::unique_ptr<fcitx::HandlerTableEntry<fcitx::EventHandler>> deactivationWatcher;
+
+    void onActivate(fcitx::Event&);
+    void onDeactivate(fcitx::Event&);
+    void onDebounceComplete(uint64_t usec);
 };
 
 class VirtualKeyboardAdapterFactory: public fcitx::AddonFactory {
